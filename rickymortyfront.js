ab0a3a0280268ec6aef2cd04ed959c6dac735d31
Flor <mariaflorenciaperezd@gmail.com>
2025-04-24 18:21:46 +0200

    funciona

--- rickymortyfront.js
+++ rickymortyfront.js
@@ -3,20 +3,22 @@ function getRickAndMortyCharacters() {
     const DivRenderCharacter = document.getElementById('renderCharacter');
 
     const nameToLowercase = RyMNameInput.value.toLowerCase();
+    //console.log(nameToLowercase);
     
-    fetch(`http://localhost:3000/characters${nameToLowercase}`)
+    fetch(`http://localhost:3000/characters/${nameToLowercase}`)
+    
     .then(response => response.json())
     .then(data => {
         if (data.error) {
             DivRenderCharacter.innerHTML = `<p>El personaje seleccionada no tiene coincidencia</p>`;
             return;
         } else {
-        const { name, Status, Species, gender, origin, image} = data;
+        const { name, status, species, gender, origin, image} = data;
         DivRenderCharacter.innerHTML = `
                 <h2>${name}</h2>
                 <ul>
-                <li>Status: ${Status}</li>
-                <li>Species: ${Species}</li>
+                <li>Status: ${status}</li>
+                <li>Species: ${species}</li>
                 <li>Gender: ${gender}</li>
                 <li>Origin: ${origin.name}</li>
                 <li>URL: ${origin.url}</li>
